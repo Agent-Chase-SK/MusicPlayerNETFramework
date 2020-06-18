427b71c4df1007e203db7731365ad220ea33cc95
Jakub Suslik <xsuslik@fi.muni.cz>
2020-06-18 19:18:21 +0200

    CodeMaid shell cleanup

--- MusicPlayer/Shell/App.xaml.cs
+++ MusicPlayer/Shell/App.xaml.cs
@@ -4,12 +4,6 @@ using MusicPlayerModule.Views;
 using Prism.Ioc;
 using Prism.Unity;
 using Shell.Views;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Shell
--- MusicPlayer/Shell/ViewModels/ShellViewModel.cs
+++ MusicPlayer/Shell/ViewModels/ShellViewModel.cs
@@ -1,14 +1,9 @@
 ﻿using Prism.Mvvm;
 using Prism.Regions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shell.ViewModels
 {
-    class ShellViewModel : BindableBase
+    internal class ShellViewModel : BindableBase
     {
         public ShellViewModel(IRegionViewRegistry regionViewRegistry)
         {
